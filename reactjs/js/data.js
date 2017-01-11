02d09c6469e53ef2852512eab46e79025ea07235
aleksandrnikolaev <SNikolaev98@mail.ru>
2017-01-11 22:20:52 +0200

    cjh

---
+++ reactjs/js/data.js
@@ -0,0 +1,17 @@
+var model = [
+    {   id: 1,
+        name: 'Google',
+        url: 'google.com',
+        desc: "empty"
+    },
+    {   id: 2,
+        name: 'facebook',
+        url: 'facebook.com',
+        desc: "empty facebook"
+    },
+    {   id: 3,
+        name: 'Yahoo',
+        url: 'yahoo.com',
+        desc: "empty yahoo"
+    }
+];
